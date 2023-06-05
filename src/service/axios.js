08bd142c99f08755a26ca6290db5bd214e2e71df
NKV <vi-nguyen@sens-vn.com>
2023-06-06 00:29:17 +0700

    fix origin

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const baseAxios = axios.create({
-  // baseURL: "https://api.centalki.com/v1",
-  baseURL: "http://localhost:4000/v1",
+  baseURL: "https://api.centalki.com/v1",
+  // baseURL: "http://localhost:4000/v1",
 });
 
 baseAxios.interceptors.response.use(
