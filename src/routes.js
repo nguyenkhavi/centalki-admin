b12fd9ae8235ef9844a3cd662f3f62118291a7a9
NKV <vi-nguyen@sens-vn.com>
2023-06-28 11:57:50 +0700

    fix sign in

--- src/routes.js
+++ src/routes.js
@@ -68,12 +68,12 @@ const routes = [
   //   icon: <MdPerson className="h-6 w-6" />,
   //   component: <Profile />,
   // },
-  // {
-  //   name: "Sign In",
-  //   layout: "/auth",
-  //   path: "sign-in",
-  //   icon: <MdLock className="h-6 w-6" />,
-  //   component: <SignIn />,
-  // },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "sign-in",
+    icon: <MdLock className="h-6 w-6" />,
+    component: <SignIn />,
+  },
 ];
 export default routes;
